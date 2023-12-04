4ac78d8c616345db160635cc393dad1bb0a7e6bc
Nadia <nadia.shevtsova@oceanshq.com>
2023-12-04 13:14:41 +1000

    276. Adding User Logout

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -8,9 +10,20 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(
+              Icons.exit_to_app,
+            ),
+            color: Theme.of(context).colorScheme.primary,
+          )
+        ],
       ),
-      body: Center(
-        child: const Text(
+      body: const Center(
+        child: Text(
           'Logged in!',
         ),
       ),
